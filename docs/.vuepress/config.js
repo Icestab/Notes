136243ed8cb9cbc06c4c783d631067f74ee3046b
Icestab <dengyue35@qq.com>
2021-04-03 19:14:35 +0800

    修改介绍

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,22 +3,23 @@ module.exports = {
   description: 'Accept what was and what is, and you\'ll more positive energy to pursue what will be.',
   plugins: {
     '@vuepress/search': {
-        search: true, //默认false
-        searchMaxSuggestions: 10 // 默认是5
+      search: true, //默认false
+      searchMaxSuggestions: 10 // 默认是5
     }
   },
   themeConfig: {
     // 添加导航栏
     nav: [
-        { text: '主页', link: '/' },
-        { text: '指南', link: '/guide/' },
-        { text: '生活', link: '/life/'},
-        { text: '学习',
-            items: [
-                { text: '英语', link: '/study/english/english01' },
-                { text: '数学', link: '/study/math/math01' },
-            ]
-        }
+      { text: '主页', link: '/' },
+      { text: '指南', link: '/guide/' },
+      { text: '生活', link: '/life/' },
+      {
+        text: '学习',
+        items: [
+          { text: '前端', link: '/study/english/english01' },
+          { text: '文学', link: '/study/math/math01' },
+        ]
+      }
     ],
   }
 }
